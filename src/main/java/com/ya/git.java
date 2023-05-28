0a2d02b319e7eac91fa302953db5148528ea725e
yagote <3525013968@qq.com>
2023-05-28 11:13:45 +0800

    first-commit

---
+++ src/main/java/com/ya/git.java
@@ -0,0 +1,10 @@
+package com.ya;
+
+/**
+ * @author yagote    create 2023/5/28 11:04
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+}
